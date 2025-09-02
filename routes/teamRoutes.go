9e0437d653ace1245309a3605d643bbf625954ab
Harsh Vardhan <harshvardhan.p27@gmail.com>
2025-09-02 17:22:14 +0530

    auth:users collection creation

--- routes/teamRoutes.go
+++ routes/teamRoutes.go
@@ -9,7 +9,7 @@ import (
 )
 
 func RegisterTeamRoutes(r *mux.Router) {
-	r.Handle("/teams", middleware.AuthMiddleware(http.HandlerFunc(controllers.CreateTeam))).Methods("POST")
+	r.Handle("/teams/create", middleware.AuthMiddleware(http.HandlerFunc(controllers.CreateTeam))).Methods("POST")
 	r.Handle("/teams/join", middleware.AuthMiddleware(http.HandlerFunc(controllers.JoinTeam))).Methods("PUT")
 	r.Handle("/teams/get", middleware.AuthMiddleware(http.HandlerFunc(controllers.GetTeam))).Methods("GET")
 }
