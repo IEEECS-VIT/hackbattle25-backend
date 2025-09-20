6e07cf5825e3eac7d34725a595e81c6e1042bcc0
Aryan Jain <aryanrameshjain@gmail.com>
2025-09-20 22:42:31 +0530

    remove check
    Signed-off-by: Aryan Jain <aryanrameshjain@gmail.com>

--- controllers/authController.go
+++ controllers/authController.go
@@ -29,7 +29,6 @@ func SignIn(authClient *auth.Client, firestoreClient *firestore.Client) http.Han
 
 		idToken := parts[1]
 
-		// 2. Verify the Google ID token using Firebase Auth
 		token, err := authClient.VerifyIDToken(context.Background(), idToken)
 		if err != nil {
 			log.Printf("error verifying ID token: %v\n", err)
@@ -37,42 +36,53 @@ func SignIn(authClient *auth.Client, firestoreClient *firestore.Client) http.Han
 			return
 		}
 
-		// 3. Check if the user's email exists in the 'users' collection in Firestore
-		userEmail := strings.ToLower(token.Claims["email"].(string)) // normalize to lowercase
+		userEmail := strings.ToLower(token.Claims["email"].(string)) // doc ID
 		ctx := context.Background()
 
 		userRef := firestoreClient.Collection("users").Doc(userEmail)
-		doc, err := userRef.Get(ctx)  //get the user document
+		doc, err := userRef.Get(ctx)
 
 		if status.Code(err) == codes.NotFound {
-			// no document found, user is not registered
-			log.Printf("Sign-in failed: email '%s' not found in registrations.", userEmail)
-			http.Error(w, "User has not registered for the event.", http.StatusNoContent)
+			_, err := userRef.Set(ctx, map[string]interface{}{
+				"email":  userEmail,
+				"TeamID": nil,
+			})
+			if err != nil {
+				log.Printf("Error creating Firestore user doc: %v", err)
+				http.Error(w, "Internal server error", http.StatusInternalServerError)
+				return
+			}
+			log.Printf("Created new user document for %s", userEmail)
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(map[string]bool{
+				"isInTeam": false,
+			})
 			return
 		}
+
 		if err != nil {
 			log.Printf("Error checking Firestore: %v", err)
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 			return
 		}
 
-		var isInTeam bool = false
-		TeamID , err := doc.DataAt("TeamID")
-
-		if(err == nil && TeamID != nil){
+		var isInTeam bool
+		TeamID, err := doc.DataAt("TeamID")
+		if err == nil && TeamID != nil {
 			isInTeam = true
-		} else{
+		} else {
 			isInTeam = false
 		}
+
 		log.Printf("User %s found in Firestore. TeamID: %v", userEmail, TeamID)
 
-		// 5. User is registered and has a user document, sign-in is successful
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(map[string]bool{
-			"isInTeam" : isInTeam,
+			"isInTeam": isInTeam,
 		})
-		log.Printf("Successfully signed in user: %s", userEmail)
-
 	}
 }
+
